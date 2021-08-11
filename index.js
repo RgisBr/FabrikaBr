63fab9fd634ae0b9de60349dd87aa3022787c4e1
FabrikaBr <67053790+FabrikaBr@users.noreply.github.com>
2021-08-11 20:02:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="FabrikaBr" />
+    <h2>Bem vindo!</h2>
+    <p>FabrikaBr é uma plataforma dedicada ao desenvolvimento de produtos e serviços tecnológicos.</p>
+    <p>Fornecemos soluções para o desenvolvimento de protótipos baseados em IA e IoT, automação de processos, monitoramento e relatórios gerenciais.
+       Nossa metodologia de trabalho descentralizado, permitem que nossos processos sejam executados por uma equipe multidisciplinar remota com confiança e escalabilidade.  </p>
+    
+    <p>
+      <Link to="/page-2/">Projetos</Link> <br />
+      <p>Desenvolva seu projeto, aprenda utilizar os artefatos de controle e monitoramento </p>
+      <Link to="/using-typescript/">Blockchain</Link><br />
+      <p>Saiba como a tecnologia Blockchain pode ajudar você.</p>
+      <Link to="/About/">About</Link>
+      <p>Saiba mais sobre nossos produtos.</p>
+    </p>
+  </Layout>
+)
+
+export default IndexPage
