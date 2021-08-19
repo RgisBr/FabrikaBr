19a52ab5031b077e94b203f7040eb8357ab4cc7b
FabrikaBr <67053790+FabrikaBr@users.noreply.github.com>
2021-08-19 14:21:03 -0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `FABRIKABR`,
+    description: `FabrikaBr collective of students and Computer Engineering professionals.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `https://fabrikabr.io/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
