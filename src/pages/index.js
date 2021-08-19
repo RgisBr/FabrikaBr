c842fb534c1042ddefe1cd96a291a5f45766585e
FabrikaBr <67053790+FabrikaBr@users.noreply.github.com>
2021-08-19 13:15:44 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,15 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="FabrikaBr" />
-    <h2>Bem vindo!</h2>
+    <h2>Welcome!</h2>
     <p>FabrikaBr is an embryonic platform formed by professionals and students of Computer Engineering. Our goal is to develop technological products and services that can improve people's lives..</p>
-    <p>Fornecemos soluções para o desenvolvimento de protótipos baseados em IA e IoT, automação de processos, monitoramento e relatórios gerenciais.
-       Nossa metodologia de trabalho descentralizado, permitem que nossos processos sejam executados por uma equipe multidisciplinar remota com confiança e escalabilidade.  </p>
+    <p>We are enthusiastic about new technologies and their applications, mainly IA, IOT and Blockchain.
+We believe in community economic models and decentralized governance. Our applications under development are based on process improvement, enabling greater savings in resources and consequently labor efficiency. Our work methodology follows a model that values human interaction following the best practices..  </p>
     
     <p>
-      <Link to="/page-2/">Projetos</Link> <br />
-      <p>Desenvolva seu projeto, aprenda utilizar os artefatos de controle e monitoramento </p>
+      <Link to="/page-2/">Roadmap</Link> <br />
+      <p>2021- 
+  Team design and formation </p>
       <Link to="/using-typescript/">Blockchain</Link><br />
       <p>Saiba como a tecnologia Blockchain pode ajudar você.</p>
       <Link to="/About/">About</Link>
