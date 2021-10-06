89ed05b1c5d99f8be0739537f5e4ba60e53d26d3
FabrikaBr <67053790+FabrikaBr@users.noreply.github.com>
2021-10-06 17:48:59 -0300

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,10 @@
+
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
 <body>
   
   <!-- Start of ChatBot (www.chatbot.com) code -->
