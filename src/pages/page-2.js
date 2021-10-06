2e19a0a556d8946db5b616959088176ac788e867
FabrikaBr <67053790+FabrikaBr@users.noreply.github.com>
2021-10-06 17:01:18 -0300

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -11,6 +11,17 @@ const SecondPage = () => (
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
-)
 
+<!-- Start of ChatBot (www.chatbot.com) code -->
+<script type="text/javascript">
+    window.__be = window.__be || {};
+    window.__be.id = "615de1f80ceb130007ead5a5";
+    (function() {
+        var be = document.createElement('script'); be.type = 'text/javascript'; be.async = true;
+        be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(be, s);
+    })();
+</script>
+<!-- End of ChatBot code -->
+)
 export default SecondPage
