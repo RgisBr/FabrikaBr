a987617495ee8a313eb9f392580050a3505ee9b5
FabrikaBr <67053790+FabrikaBr@users.noreply.github.com>
2021-10-06 17:17:29 -0300

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,9 +6,19 @@ import Seo from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <Seo title="Roadmap" />
+    <h1>Roadmap </h1>
+    <p>Tasks</p>
+  <p>  2021- 1Q/2Q Design and staff training.</p>
+<p> Done</p>
+  <p> 2021- 3Q/4Q organizational structure, laboratory testing, research, fundraising and development partners projects..</p>
+<p>Done</p>
+  <p> 2022- 1Q/2Q Reviewing working structure and TESTNET the Network BSC.</p>
+<p>Done</p>
+  <p>  2022- 3Q/4Q lessons learned KB, creating business environment, planning for 1Q/2Q 2022 and 3Q/4Q 2022.</p>
+ <p> Done </p>
+  
+  
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 
