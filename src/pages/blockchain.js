b782b55cb667e9ff88172ef64d3314482fc41243
FabrikaBr <67053790+FabrikaBr@users.noreply.github.com>
2022-03-17 13:29:12 -0300

    Update blockchain.js

--- src/pages/blockchain.js
+++ src/pages/blockchain.js
@@ -8,9 +8,16 @@ const SecondPage = () => (
   <Layout>
     <Seo title="Blockchain" />
     <h1>About </h1>
-    <p>
-  Blockchain technology is known for being responsible for recording transactions
-transactions on the Bitcoin network. The premises of this technology are decentralization, data immutability, transparency, and scalability. These characteristics have contributed to the adoption of Blockchain by businesses, industries, financial institutions, and governments. In 2017 some companies in the retail sector, in partnership with IBM, developed projects to implement Blockchain technology. The initiative aims to optimize monitoring and control processes, collecting information about products at each stage of the supply chain, meeting legal and environmental requirements. Our proposal is to offer a service platform to act in the cryptography market, offering specialized services for companies and Blockchain technology users, besides developing Blockchain solutions based on information architecture, information security and data distribution. Our team is composed of technology professionals such as Programmers, Web Designers, Project Analysts, and Project Manager. The organizational system is based on Holocracy, this system allows governance to be distributed to the teams, enabling employees to make proposals or define rules through voting.
+    <p> Blockchain technology is known for being responsible for recording transactions on the Bitcoin network. </p> 
+    <p>The premises of this technology are decentralization, data immutability, transparency, and scalability. </p>
+   <p>These characteristics have contributed to the adoption of Blockchain by businesses, industries, financial institutions, and governments.  </p>
+   <p>In 2017 some companies in the retail sector, in partnership with IBM, developed projects to implement Blockchain technology.  </p>
+   <p>The initiative aims to optimize monitoring and control processes, collecting information about products at each stage of the
+  supply chain, meeting legal and environmental requirements.  </p>
+   <p>Our proposal is to offer a service platform to act in the cryptography market, offering specialized services for companies and Blockchain technology users, 
+  besides developing Blockchain solutions based on information architecture, information security and data distribution.  </p>
+   <p>Our team is composed of technology professionals such as Programmers, Web Designers, Project Analysts, and Project Manager.  </p>
+   <p>The organizational system is based on Holocracy, this system allows governance to be distributed to the teams, enabling employees to make proposals or define rules through voting.
 
  </p>
   <p> 
