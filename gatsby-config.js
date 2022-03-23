3c8b82102ebea2645b2a371258b6e2f991ea6975
FabrikaBr <67053790+FabrikaBr@users.noreply.github.com>
2022-03-22 22:07:58 -0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,15 @@ module.exports = {
     siteUrl: `https://fabrikabr.io/`,
   },
   plugins: [
+     {
+    resolve: `gatsby-plugin-nprogress`,
+    options: {
+      // Setting a color is optional.
+      color: `tomato`,
+      // Disable the loading spinner.
+      showSpinner: false,
+    },
+  },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
