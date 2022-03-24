c0d2f5b96a195d8bed7726b7c48e878fe8eca08d
FabrikaBr <67053790+FabrikaBr@users.noreply.github.com>
2022-03-24 19:10:12 -0300

    Update io.js

--- src/pages/io.js
+++ src/pages/io.js
@@ -7,33 +7,32 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 
+ const SecondPage = () => (
   <Layout>
     <Seo title="About" />
     <h1>About </h1>
-    const SecondPage = () => (
- 
-     <Layout>
-    <Seo title="Blockchain" />
-    <h1>About Blockchain</h1>
-    <p> Blockchain technology is known for being responsible for recording transactions on the Bitcoin network. </p> 
-    <p>The premises of this technology are decentralization, data immutability, transparency, and scalability. </p>
-   <p>These characteristics have contributed to the adoption of Blockchain by businesses, industries, financial institutions, and governments.  </p>
-   <p>In 2017 some companies in the retail sector, in partnership with IBM, developed projects to implement Blockchain technology.  </p>
-   <p>The initiative aims to optimize monitoring and control processes, collecting information about products at each stage of the
-  supply chain, meeting legal and environmental requirements.  </p>
-   <p>Our proposal is to offer a service platform to act in the cryptography market, offering specialized services for companies and Blockchain technology users, 
-  besides developing Blockchain solutions based on information architecture, information security and data distribution.  </p>
-   <p>Our team is composed of technology professionals such as Programmers, Web Designers, Project Analysts, and Project Manager.  </p>
-   <p>The organizational system is based on Holocracy, this system allows governance to be distributed to the teams, enabling employees to make proposals or define rules through voting.
-
- </p>
+    <p>Blockchain technology is known to be responsible for recording transactions on the Bitcoin network. 
+       The premises of this technology are decentralization, data immutability, transparency and scalability. 
+       These characteristics have contributed to the adoption of the Blockchain by companies, industries, financial institutions and governments.
+       In 2017, some companies in the retail sector, in partnership with IBM, developed projects to implement the Blockchain technology. 
+       The initiative aims to optimize monitoring and control processes, collecting information about products at each stage of the supply chain, meeting legal and environmental requirements.
+       Our proposal is to offer a service platform to operate in the cryptography market, offering specialized services for companies and users of Blockchain technology, 
+       in addition to developing Blockchain solutions based on information architecture, information security and data distribution. 
+       Our industry is a market of US$2.3 trillion in capitalization (Oct/21), 3x larger than the capitalization of B3 US$773 billion dollars (01/21). 
+       Our team consists of technology professionals such as Programmers, Web Designers, Project Analysts, and Project Manager. 
+       The organizational system is based on Holocracy, this system allows governance to be distributed to teams, enabling employees to prepare proposals or definitions of rules through voting. 
+       The proposed business model follows the requirements of the Brazilian Complementary Law (LC) No. 182/2021 The legal framework for startups 
+        </p>
   <p> 
-  
+  <a class="e-widget no-button" href="https://gleam.io/S3I1I/fabrikabrio-fbkbr-airdrop" rel="nofollow">FabrikaBr.IO FBKBR Airdrop</a>
+<script type="text/javascript" src="https://widget.gleamjs.io/e.js" async="true"></script>
   </p>
   
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 
 
+)
+export default SecondPage
 )
 export default 
