8af06bc4c754f34f9dbf52ef427667b7e5ecb96a
FabrikaBr <67053790+FabrikaBr@users.noreply.github.com>
2022-03-23 21:23:52 -0300

    Update io.js

--- src/pages/io.js
+++ src/pages/io.js
@@ -1,1 +1,25 @@
 
+
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const SecondPage = () => (
+  <Layout>
+    <Seo title="About" />
+    <h1>About </h1>
+    <p>
+        </p>
+  <p> 
+  <a class="e-widget no-button" href="https://gleam.io/S3I1I/fabrikabrio-fbkbr-airdrop" rel="nofollow">FabrikaBr.IO FBKBR Airdrop</a>
+<script type="text/javascript" src="https://widget.gleamjs.io/e.js" async="true"></script>
+  </p>
+  
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+
+
+)
+export default SecondPage
