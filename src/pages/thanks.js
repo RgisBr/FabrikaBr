53286879c482e8dca9acf30828c7b7cf479430c4
FabrikaBr <67053790+FabrikaBr@users.noreply.github.com>
2022-03-26 00:06:45 -0300

    Update thanks.js

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,4 +1,3 @@
-
 import * as React from "react"
 import { Link } from "gatsby"
 
@@ -6,45 +5,29 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 
- const SecondPage = () => (
-  <Layout>
-    <Seo title="Thank you for contacting us!" />
-    <h1>Thank you for contacting us!</h1>
-    
-  <p>One of our analysts will soon answer your questions.</p>
-  <p> 
- <table style="height: 18px; width: 100%; border-collapse: collapse;" border="1">
-<tbody>
-<tr style="height: 18px;">
-<td style="width: 50%; height: 18px;">&nbsp;</td>
-<td style="width: 50%; height: 18px;">&nbsp;</td>
-</tr>
-</tbody>
-</table> 
-  
-  
-  </p>
-  <p> </p>
-  <p> </p>
-  <p> </p>
-  <p> </p>
-  <p> </p>
-  <p> </p>
-  <p> </p>
-  <p> </p>
-  <p> </p>
-  <p> </p>
-  <p> </p>
-  <p> </p>
-  <p> </p>
-  <p> </p>
-  
-  
- <p><Link to="/">Go back to the homepage</Link> </p>
-  
-  
-  
-  </Layout>
+const SecondPage = () => (
+    <Layout>
+        <Seo title="Thank you for contacting us!" />
+        <h1>Thank you for contacting us!</h1>
+
+        <p>One of our analysts will soon answer your questions.</p>
+        
+            <table style="height: 18px; width: 100%; border-collapse: collapse;" border="1">
+                <tbody>
+                    <tr style="height: 18px;">
+                        <td style="width: 50%; height: 18px;">&nbsp;</td>
+                        <td style="width: 50%; height: 18px;">&nbsp;</td>
+                    </tr>
+                </tbody>
+            </table>
+        
+
+
+        <p><Link to="/">Go back to the homepage</Link> </p>
+
+
+
+    </Layout>
 
 
 )
