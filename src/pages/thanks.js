fc441ebd5e94f44351ba8ca602e28b7ce238124d
FabrikaBr <67053790+FabrikaBr@users.noreply.github.com>
2022-03-26 00:01:51 -0300

    Update thanks.js

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -12,7 +12,18 @@ import Seo from "../components/seo"
     <h1>Thank you for contacting us!</h1>
     
   <p>One of our analysts will soon answer your questions.</p>
-  <p> </p>
+  <p> 
+ <table style="height: 18px; width: 100%; border-collapse: collapse;" border="1">
+<tbody>
+<tr style="height: 18px;">
+<td style="width: 50%; height: 18px;">&nbsp;</td>
+<td style="width: 50%; height: 18px;">&nbsp;</td>
+</tr>
+</tbody>
+</table> 
+  
+  
+  </p>
   <p> </p>
   <p> </p>
   <p> </p>
