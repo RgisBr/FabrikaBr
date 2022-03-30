8d2972182ff2a92a2f47e38adf818ca55c3b4076
FabrikaBr <67053790+FabrikaBr@users.noreply.github.com>
2022-03-30 17:12:22 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import * as React from "react"
 import { Link } from "gatsby"
+//***
+import * as React from "react"
+// Import from a CSS file in your src
+import "../styles/index.css"
+// Import from an installed package
+import "bootstrap/dist/css/bootstrap.min.css"
+//***
+export default function HomePage() {
+  return <div>I'm styled by bootstrap & src/styles/index.css</div>
+}
+
+
 
 
 import Layout from "../components/layout"
